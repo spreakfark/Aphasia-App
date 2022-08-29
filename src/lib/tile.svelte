ca1370c21b13676f50355367a6539901e49efb37
Julian Hoffmann <julian@jhgo.de>
2022-08-29 17:54:08 +0200

    adds tile component

---
+++ src/lib/tile.svelte
@@ -0,0 +1,23 @@
+<button>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        background-color: #ffffff;
+        color: #000000;
+        border: 1px #000000 solid;
+        border-radius: 0;
+        margin: -1px;
+        padding: 2rem;
+        transition: 0s;
+        transition-delay: 3s;
+        transition-duration: 0.5s;
+    }
+
+    button:active {
+        transition-delay: -1s;
+        background-color: #000000;
+        color: #ffffff;
+    }
+</style>
