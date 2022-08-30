073ecafaddc3de10a6d03a81f5ddbc1b6cdf8894
Julian Hoffmann <julian@jhgo.de>
2022-08-30 17:28:08 +0200

    adds Poppins Medium font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,14 @@
 <style>
 	@font-face {
 		font-family: 'Poppins';
+		font-weight: 700;
 		src: url('/fonts/poppins/Poppins-Bold.woff2') format('woff2'), url('/fonts/poppins/Poppins-Bold.woff') format('woff');
+	}
+
+	@font-face {
+		font-family: 'Poppins';
+		font-weight: 500;
+		src: url('/fonts/poppins/Poppins-Medium.woff2') format('woff2'), url('/fonts/poppins/Poppins-Medium.woff') format('woff');
 	}
 
     :global(body) {
