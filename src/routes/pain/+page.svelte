73bdbee851ac6f0a9dbc9df93616009fb6e48ee3
Julian Hoffmann <julian@jhgo.de>
2022-08-30 17:28:23 +0200

    adds pain intensity page

--- src/routes/pain/+page.svelte
+++ src/routes/pain/+page.svelte
@@ -4,16 +4,16 @@
 </script>
 
 <div class="grid">
-	<Tile>Head</Tile>
-	<Tile>Stomach</Tile>
-	<Tile>Chest</Tile>
-	<Tile>Back</Tile>
-	<Tile>Legs</Tile>
-	<Tile>Feet</Tile>
-	<Tile>Skin</Tile>
-	<Tile>Eyes</Tile>
-	<Tile>Shoulder</Tile>
-	<Tile>Mouth</Tile>
-	<Tile>Arm</Tile>
+	<Tile href="/pain/intensity">Head</Tile>
+	<Tile href="/pain/intensity">Stomach</Tile>
+	<Tile href="/pain/intensity">Chest</Tile>
+	<Tile href="/pain/intensity">Back</Tile>
+	<Tile href="/pain/intensity">Legs</Tile>
+	<Tile href="/pain/intensity">Feet</Tile>
+	<Tile href="/pain/intensity">Skin</Tile>
+	<Tile href="/pain/intensity">Eyes</Tile>
+	<Tile href="/pain/intensity">Shoulder</Tile>
+	<Tile href="/pain/intensity">Mouth</Tile>
+	<Tile href="/pain/intensity">Arm</Tile>
 	<Tile href="/"><BackIcon /></Tile>
 </div>
---
+++ src/routes/pain/intensity/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import BackIcon from '$lib/back-icon.svelte';
+	import Tile from '$lib/tile.svelte';
+</script>
+
+<div class="wrapper">
+    <div class="container">
+        <div class="banner">
+            <span>Rate your pain</span>
+            <span class="description">10 being the worst pain</span>
+        </div>
+    </div>
+    <div class="grid">
+        <Tile --tile-bg-color="#ffffff">1</Tile>
+        <Tile --tile-bg-color="#FFEBEB">2</Tile>
+        <Tile --tile-bg-color="#FFD7D7">3</Tile>
+        <Tile --tile-bg-color="#FFC8C8">4</Tile>
+        <Tile --tile-bg-color="#FFB6B6">5</Tile>
+        <Tile --tile-bg-color="#FF8F8F">6</Tile>
+        <Tile --tile-bg-color="#FF7A7A">7</Tile>
+        <Tile --tile-bg-color="#FF5E5E">8</Tile>
+        <Tile --tile-bg-color="#FF3A3A">9</Tile>
+        <Tile --tile-bg-color="#FF2727">10</Tile>
+        <Tile disabled />
+        <Tile href="/"><BackIcon /></Tile>
+    </div>
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+    }
+    .container {
+        width: 100%;
+    }
+    .grid {
+        height: 100%;
+    }
+    .banner {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 3vh 5vw;
+        font-size: clamp(1rem, min(4vw, 4vh), 2.75rem);
+    }
+    .description {
+        font-weight: 500;
+        font-size: clamp(1rem, min(3vw, 3vh), 2.75rem);
+    }
+</style>
