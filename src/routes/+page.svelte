2bb789041069979319ff48f0e247d479ecc4724d
Julian Hoffmann <julian@jhgo.de>
2022-08-30 14:47:59 +0200

    adds icons to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <div class="grid">
-	<Tile>Incline Bed</Tile>
-	<Tile>Lay down</Tile>
-	<Tile>Sit up</Tile>
-	<Tile>Need Medicine</Tile>
-	<Tile>Nurse or Doctor</Tile>
-	<Tile>In Pain</Tile>
-	<Tile>Food/Drink</Tile>
-	<Tile>TV</Tile>
-	<Tile>Bath</Tile>
-	<Tile>Change Bed</Tile>
-	<Tile>Emotions</Tile>
-	<Tile>Yes/No</Tile>
+	<Tile icon_href="/icons/seat-flat-angled.svg">Incline Bed</Tile>
+	<Tile icon_href="/icons/seat-flat.svg">Lay down</Tile>
+	<Tile icon_href="/icons/seat-passenger.svg">Sit up</Tile>
+	<Tile icon_href="/icons/pill.svg">Need Medicine</Tile>
+	<Tile icon_href="/icons/doctor.svg">Nurse or Doctor</Tile>
+	<Tile icon_href="/icons/lightning-bolt.svg">In Pain</Tile>
+	<Tile icon_href="/icons/food.svg">Food/Drink</Tile>
+	<Tile icon_href="/icons/television-classic.svg">TV</Tile>
+	<Tile icon_href="/icons/shower.svg">Bath</Tile>
+	<Tile icon_href="/icons/paper-roll-outline.svg">Change Bed</Tile>
+	<Tile icon_href="/icons/emoticon-angry-outline.svg" href="/emotions">Emotions</Tile>
+	<Tile href="/yes-no">Yes/No</Tile>
 </div>
