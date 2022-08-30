831dfc24c072a05e66e539b0cff1a2fc09b86d51
Julian Hoffmann <julian@jhgo.de>
2022-08-30 14:48:30 +0200

    adds emotions page

---
+++ src/routes/emotions/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+import Tile from "$lib/tile.svelte";
+
+</script>
+<div class="grid">
+	<Tile>Angry</Tile>
+	<Tile>Sad</Tile>
+	<Tile>Lonely</Tile>
+	<Tile>Confused</Tile>
+	<Tile>Happy</Tile>
+	<Tile>Worried</Tile>
+	<Tile>Scared</Tile>
+	<Tile>Have Anxiety</Tile>
+	<Tile>Tired</Tile>
+	<Tile>Depressed</Tile>
+	<Tile disabled></Tile>
+	<Tile href="/">Back</Tile>
+</div>
