237bbdfe31e5155117c339786aeb43bb36fb50b1
Julian Hoffmann <julian@jhgo.de>
2022-08-30 16:14:12 +0200

    adds back-icon component

---
+++ src/lib/back-icon.svelte
@@ -0,0 +1,19 @@
+<div class="back">
+	<svg class="back-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+		><path d="M10,9V5L3,12L10,19V14.9C15,14.9 18.5,16.5 21,20C20,15 17,10 10,9Z" /></svg
+	>
+</div>
+
+<style>
+    .back {
+        padding: 0.5rem;
+        height: clamp(1.5rem, 4vw, 2.5rem);
+        width: clamp(1.5rem, 4vw, 2.5rem);;
+    }
+    .back-icon {
+        fill: #C1C1C1;
+        height: clamp(1.5rem, 4vw, 2.5rem);
+        width: clamp(1.5rem, 4vw, 2.5rem);;
+        object-fit: contain;
+    }
+</style>
