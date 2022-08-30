47432186e95299a2dd0f1a7990a25f2b1350053b
Julian Hoffmann <julian@jhgo.de>
2022-08-30 14:48:48 +0200

    adds yes-no page

---
+++ src/routes/yes-no/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+import Tile from "$lib/tile.svelte";
+
+</script>
+
+<a class="back" href="/">Back</a>
+<div class="yes-no-page grid">
+    <Tile class="big">Yes</Tile>
+    <Tile class="big">No</Tile>
+</div>
+
+<style>
+    .grid {
+        grid-template-columns: none;
+        grid-template-rows: repeat(2, 1fr);
+    }
+
+    .yes-no-page :global(.big) {
+        font-size: 18vw;
+    }
+
+    .back {
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        z-index: 10;
+    }
+</style>
