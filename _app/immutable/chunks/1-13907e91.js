2f8ed3c43b3be06dfa785056e69e7655f3604bdb
Julian Hoffmann <julian@jhgo.de>
2022-08-30 22:44:14 +0200

    Updates

---
+++ _app/immutable/chunks/1-13907e91.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/error.svelte-62724b38.js";import"./index-bf283ca9.js";import"./singletons-48ab4738.js";import"./index-18da49c2.js";import"./paths-6cd3a76e.js";export{e as component};
