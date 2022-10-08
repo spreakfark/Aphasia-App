e1f2a7a942d26309447fde67a92520b16ad55d7e
Julian Hoffmann <julian@jhgo.de>
2022-10-08 16:04:27 +0200

    Updates

---
+++ _app/immutable/chunks/0-fd315b74.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_layout.svelte-26e50ae9.js";import"./paths-74db60bd.js";import"./index-741b9f67.js";export{r as component};
---
+++ _app/immutable/chunks/1-8fefbf40.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/error.svelte-54fb0dbe.js";import"./paths-74db60bd.js";import"./singletons-35aed605.js";import"./index-741b9f67.js";export{e as component};
